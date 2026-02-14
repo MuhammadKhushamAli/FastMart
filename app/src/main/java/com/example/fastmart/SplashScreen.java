dde162347eef9f0560b3eb9e85c204191e6c8132
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-02-15 01:04:41 +0500

    Loading barr configged with splash screen

--- app/src/main/java/com/example/fastmart/SplashScreen.java
+++ app/src/main/java/com/example/fastmart/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.example.fastmart;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -15,8 +16,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class SplashScreen extends AppCompatActivity {
     Animation top_to_current;
     Animation bottom_to_current;
+    Animation loading_bar_anim;
     ImageView logo;
     TextView slogan;
+    View loading_bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,14 +38,17 @@ public class SplashScreen extends AppCompatActivity {
     {
         logo = findViewById(R.id.logo);
         slogan = findViewById(R.id.slogan);
+        loading_bar = findViewById(R.id.loading_bar);
+
         top_to_current = AnimationUtils.loadAnimation(this, R.anim.top_to_current);
         bottom_to_current = AnimationUtils.loadAnimation(this, R.anim.bottom_to_current);
-
+        loading_bar_anim = AnimationUtils.loadAnimation(this, R.anim.loading_bar);
     }
 
     private void applyAnimation()
     {
         logo.setAnimation(top_to_current);
         slogan.setAnimation(bottom_to_current);
+        loading_bar.setAnimation(loading_bar_anim);
     }
 }
