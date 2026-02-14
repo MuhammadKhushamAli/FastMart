ca987b0c8b78e3595c32a53a966175b5ea86d4da
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-02-15 00:42:07 +0500

    Top-to-current anim add and set to logo

--- app/src/main/java/com/example/fastmart/SplashScreen.java
+++ app/src/main/java/com/example/fastmart/SplashScreen.java
@@ -1,6 +1,9 @@
 package com.example.fastmart;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
-
+    Animation top_to_current;
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,18 @@ public class SplashScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+        applyAnimation();
+    }
+
+    private void init()
+    {
+        logo = findViewById(R.id.logo);
+        top_to_current = AnimationUtils.loadAnimation(this, R.anim.top_to_current);
+    }
+
+    private void applyAnimation()
+    {
+        logo.setAnimation(top_to_current);
     }
 }
