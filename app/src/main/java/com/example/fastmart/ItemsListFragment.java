a518bc68cc44e75ae168ec0d3535e3c5d6489270
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-02-15 02:55:33 +0500

    Item Lsit Fragment created + Data File Created

---
+++ app/src/main/java/com/example/fastmart/ItemsListFragment.java
@@ -0,0 +1,53 @@
+package com.example.fastmart;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ItemsListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ItemsListFragment extends ListFragment {
+
+    public interface onItemClicked {
+        public void onItemClickListener(int index);
+    }
+
+    onItemClicked parentActivity;
+
+    public ItemsListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        parentActivity = (onItemClicked) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_items_list, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/fastmart/dataFile.java
@@ -0,0 +1,4 @@
+package com.example.fastmart;
+
+public class dataFile {
+}
