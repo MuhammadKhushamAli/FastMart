b2e7327190fd375d9be17f9b5ea53096ffcab18d
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-02-16 22:27:00 +0500

    Fragment Styles Added

--- app/src/main/java/com/example/fastmart/BuyFragment.java
+++ app/src/main/java/com/example/fastmart/BuyFragment.java
@@ -1,39 +1,49 @@
 package com.example.fastmart;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class BuyFragment extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class BuyFragment extends Fragment implements View.OnClickListener {
+    private static final String ARG_MSG = "message";
+    private String paramMsg;
+    OnClickListener parentActivity;
+    interface OnClickListener {
+        public void onClick();
+    }
 
-    private String mParam1;
-    private String mParam2;
 
     public BuyFragment() {
         // Required empty public constructor
     }
 
-    public static BuyFragment newInstance(String param1, String param2) {
+    public static BuyFragment newInstance(String paramMsg) {
         BuyFragment fragment = new BuyFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_MSG, paramMsg);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        parentActivity = (OnClickListener) context;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            paramMsg = getArguments().getString(ARG_MSG);
         }
     }
 
@@ -42,5 +52,18 @@ public class BuyFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_buy_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView message = view.findViewById(R.id.bought_item_message);
+        message.setText(paramMsg);
+        view.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        parentActivity.onClick();
     }
 }
--- app/src/main/java/com/example/fastmart/DetailedView.java
+++ app/src/main/java/com/example/fastmart/DetailedView.java
@@ -2,6 +2,7 @@ package com.example.fastmart;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,14 +11,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
-public class DetailedView extends AppCompatActivity {
+public class DetailedView extends AppCompatActivity implements BuyFragment.OnClickListener {
     ImageView detailedImage;
     TextView name;
     TextView price;
     TextView description;
     TextView model;
     TextView color;
+    Button buyButton;
+
+    FragmentManager fragManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,40 +34,71 @@ public class DetailedView extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         Intent i = getIntent();
 
         int imageID = i.getIntExtra(KeyUtils.imageIDKey, 0);
-        String name = i.getStringExtra(KeyUtils.nameKey);
-        String price = i.getStringExtra(KeyUtils.priceKey);
-        String desc = i.getStringExtra(KeyUtils.descriptionKey);
-        String model = i.getStringExtra(KeyUtils.modelKey);
-        String color = i.getStringExtra(KeyUtils.colorKey);
+        String itemName = i.getStringExtra(KeyUtils.nameKey);
+        String itemPrice = i.getStringExtra(KeyUtils.priceKey);
+        String itemDesc = i.getStringExtra(KeyUtils.descriptionKey);
+        String itemModel = i.getStringExtra(KeyUtils.modelKey);
+        String itemColor = i.getStringExtra(KeyUtils.colorKey);
 
         initAndSetup(
                 imageID,
-                name,
-                price,
-                desc,
-                model,
-                color
+                itemName,
+                itemPrice,
+                itemDesc,
+                itemModel,
+                itemColor
         );
 
+        fragManager = getSupportFragmentManager();
+        StringBuilder String = new StringBuilder();
+        String.append("You are going to buy ").append(itemName).append(" in ").append(itemColor)
+                .append(" color for ").append(itemPrice);
+
+        BuyFragment buyFragment = BuyFragment.newInstance(String.toString());
+
+        buyButton.setOnClickListener((v) -> {
+            fragManager.beginTransaction()
+                    .replace(R.id.confirmation_popup, buyFragment)
+                    .commit();
+        });
     }
 
-    private void initAndSetup(int imgID, String itemName, String itemPrice, String itemDesc, String itemModel, String itemColor)
+    private void initAndSetup(
+            int imageID,
+            String itemName,
+            String itemPrice,
+            String itemDesc,
+            String itemModel,
+            String itemColor
+    )
     {
+
+
         detailedImage = findViewById(R.id.detailed_item_image);
         name = findViewById(R.id.detailed_item_name);
         price = findViewById(R.id.detailed_item_price);
         description = findViewById(R.id.detailed_item_description);
         model = findViewById(R.id.detailed_item_model);
         color = findViewById(R.id.detailed_item_color);
+        buyButton = findViewById(R.id.buy_button);
 
-        detailedImage.setImageResource(imgID);
+        detailedImage.setImageResource(imageID);
         name.setText(itemName);
         price.setText(itemPrice);
         description.setText(itemDesc);
         model.setText(itemModel);
         color.setText(itemColor);
     }
+
+
+
+    @Override
+    public void onClick() {
+
+    }
+
 }
