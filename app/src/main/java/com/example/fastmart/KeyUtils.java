6fcb0b60c724a86e622602057e2324e027018252
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-02-16 18:35:35 +0500

    Key Utils Added

---
+++ app/src/main/java/com/example/fastmart/KeyUtils.java
@@ -0,0 +1,8 @@
+package com.example.fastmart;
+
+public class KeyUtils {
+    public static final String imageIDKey = "image_key";
+    public static final String nameKey = "name_key";
+    public static final String priceKey = "price_key";
+    public static final String descriptionKey = "description_key";
+}
