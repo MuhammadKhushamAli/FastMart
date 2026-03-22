4fe855b64b04f823165494ce118899eaf367b381
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-03-22 16:51:09 +0500

    My Activity Cleanup

--- app/src/main/java/com/example/fastmart/MainActivity.java
+++ app/src/main/java/com/example/fastmart/MainActivity.java
@@ -18,17 +18,8 @@ import androidx.fragment.app.FragmentManager;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements ItemsListFragment.onItemClicked, DealOfDayFragment.onClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    FragmentManager fragManager;
-    View dodFrag;
-
-    // Dod Segment
-
-
-    // Item List
-    ArrayList<CardView> itemCardsList;
-    View listFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,55 +31,5 @@ public class MainActivity extends AppCompatActivity implements ItemsListFragment
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        fragManager = getSupportFragmentManager();
-
-        // Deal of the Day Segment
-        DealOfDayFragment dealOfDayFragment = DealOfDayFragment.newInstance(
-                R.drawable.dod,
-                DataFile.dodCatagory,
-                DataFile.dodName,
-                DataFile.dodPrice,
-                DataFile.dodDescription
-        );
-
-        fragManager.beginTransaction()
-                .replace(R.id.dod_banner, dealOfDayFragment)
-                .show(fragManager.findFragmentById(R.id.home_item_list))
-                .commit();
-
-    }
-
-    @Override
-    public void onItemClickListener(int index) {
-        DataFile.ItemCard card = DataFile.items.get(index);
-        Intent i = new Intent(MainActivity.this, DetailedView.class);
-        i.putExtra(KeyUtils.imageIDKey, card.imageID);
-        i.putExtra(KeyUtils.nameKey, card.name);
-        i.putExtra(KeyUtils.priceKey, card.price);
-        i.putExtra(KeyUtils.descriptionKey, card.description);
-        i.putExtra(KeyUtils.modelKey, card.model);
-        i.putExtra(KeyUtils.colorKey, card.color);
-
-        startActivity(i);
-    }
-
-    @Override
-    public void onItemClick(View view) {
-        ImageView image = view.findViewById(R.id.deal_of_the_day_image);
-        int imageKey = (int) image.getTag();
-        TextView name = view.findViewById(R.id.dod_item_name);
-        TextView price = view.findViewById(R.id.dod_item_price);
-        TextView description = view.findViewById(R.id.dod_item_description);
-
-        Intent i = new Intent(MainActivity.this, DetailedView.class);
-
-        i.putExtra(KeyUtils.imageIDKey,imageKey);
-        i.putExtra(KeyUtils.nameKey, name.getText());
-        i.putExtra(KeyUtils.priceKey, price.getText());
-        i.putExtra(KeyUtils.descriptionKey, description.getText());
-        i.putExtra(KeyUtils.modelKey, DataFile.dodModel);
-        i.putExtra(KeyUtils.colorKey, DataFile.dodColor);
-
-        startActivity(i);
     }
 }
