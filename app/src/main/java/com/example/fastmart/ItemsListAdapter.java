c8d6e3a8f2aa6992942e5a3e1bd29edcba0a3f8d
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-03-24 07:15:40 +0500

    ItemList Recycle Adapter Added

---
+++ app/src/main/java/com/example/fastmart/ItemsListAdapter.java
@@ -0,0 +1,65 @@
+package com.example.fastmart;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class ItemsListAdapter extends RecyclerView.Adapter<ItemsListAdapter.ItemHolder> {
+    Context context;
+    ArrayList<Item> itemArrayList;
+
+    public ItemsListAdapter(Context context, ArrayList<Item> itemArrayList) {
+        this.context = context;
+        this.itemArrayList = itemArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_card, parent, false);
+        return new ItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        Item item = itemArrayList.get(position);
+        holder.itemImage.setImageResource(item.getImageID());
+        holder.itemName.setText(item.getName());
+        holder.itemPrice.setText(item.getPrice());
+        holder.itemModel.setText(item.getModel());
+        holder.itemColor.setText(item.getColor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemArrayList.size();
+    }
+
+    public static class ItemHolder extends RecyclerView.ViewHolder {
+        ImageView itemImage;
+        TextView itemPrice;
+        TextView itemName;
+        TextView itemModel;
+        TextView itemColor;
+
+        public ItemHolder(@NonNull View itemView) {
+            super(itemView);
+            itemImage = itemView.findViewById(R.id.item_image);
+            itemPrice = itemView.findViewById(R.id.item_price);
+            itemName = itemView.findViewById(R.id.item_name);
+            itemModel = itemView.findViewById(R.id.item_model);
+            itemColor = itemView.findViewById(R.id.item_color);
+        }
+    }
+}
