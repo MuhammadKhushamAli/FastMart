f149570f2a0a9cf895c2c759858b6618502526e7
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-03-24 07:50:10 +0500

    Carousel Adapter ad styles addED

--- app/src/main/java/com/example/fastmart/CarouselViewPagerAdapter.java
+++ app/src/main/java/com/example/fastmart/CarouselViewPagerAdapter.java
@@ -1,4 +1,60 @@
 package com.example.fastmart;
 
-public class CarouselViewPagerAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CarouselViewPagerAdapter extends RecyclerView.Adapter<CarouselViewPagerAdapter.CarouselViewHolder> {
+    Context context;
+    ArrayList<Item> itemArrayList;
+
+    @NonNull
+    @Override
+    public CarouselViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.carousel_layout, parent, false);
+        return new CarouselViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CarouselViewHolder holder, int position) {
+        Item carouselItem = itemArrayList.get(position);
+        holder.carouselItemImage.setImageResource(carouselItem.getImageID());
+        holder.carouselItemCategory.setText(carouselItem.getCategory());
+        holder.carouselItemDiscountedPrice.setText(carouselItem.getDiscountedPrice());
+        holder.carouselItemOriginalPrice.setText(carouselItem.getPrice());
+        holder.carouselItemName.setText(carouselItem.getName());
+        holder.carouselItemDesc.setText(carouselItem.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemArrayList.size();
+    }
+
+    public static class CarouselViewHolder extends RecyclerView.ViewHolder {
+        ImageView carouselItemImage;
+        TextView carouselItemCategory;
+        TextView carouselItemDiscountedPrice;
+        TextView carouselItemOriginalPrice;
+        TextView carouselItemName;
+        TextView carouselItemDesc;
+
+        public CarouselViewHolder(@NonNull View itemView) {
+            super(itemView);
+            carouselItemImage = itemView.findViewById(R.id.carousel_image);
+            carouselItemCategory = itemView.findViewById(R.id.carousel_category);
+            carouselItemDiscountedPrice = itemView.findViewById(R.id.carousel_discounted_price);
+            carouselItemOriginalPrice = itemView.findViewById(R.id.carousel_original_price);
+            carouselItemName = itemView.findViewById(R.id.carousel_item_name);
+            carouselItemDesc = itemView.findViewById(R.id.carousel_item_description);
+        }
+    }
 }
