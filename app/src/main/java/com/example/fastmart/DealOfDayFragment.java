31a305409349451e3b68551863b4098aed965d08
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-03-24 14:36:14 +0500

    DOD and Home Frag Added

--- app/src/main/java/com/example/fastmart/DealOfDayFragment.java
+++ app/src/main/java/com/example/fastmart/DealOfDayFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,42 +16,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DealOfDayFragment extends Fragment {
-
-    ImageView dodImage;
-    TextView dodCategory;
-    TextView dodName;
-    TextView dodPrice;
-    TextView dodDescription;
-
-
-    private static final String ARG_IMAGE_KEY = "image_key";
-    private static final String ARG_CATEGORY = "item_category";
-    private static final String ARG_NAME = "item_name";
-    private static final String ARG_PRICE = "item_price";
-    private static final String ARG_DESCRIPTION = "item_description";
-
-
-    int paramImageKey;
-    String paramCategory;
-    String paramName;
-    String paramPrice;
-    String paramDescription;
-
-    interface onClickListener {
-        public void onItemClick(View v);
-    }
-    public static DealOfDayFragment newInstance(int imageKey, String category, String name, String price, String description) {
-        DealOfDayFragment fragment = new DealOfDayFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_IMAGE_KEY, imageKey);
-        args.putString(ARG_CATEGORY, category);
-        args.putString(ARG_NAME, name);
-        args.putString(ARG_PRICE, price);
-        args.putString(ARG_DESCRIPTION, description);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     public DealOfDayFragment() {
         // Required empty public constructor
     }
@@ -57,47 +23,29 @@ public class DealOfDayFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            paramImageKey = getArguments().getInt(ARG_IMAGE_KEY);
-            paramCategory = getArguments().getString(ARG_CATEGORY);
-            paramName = getArguments().getString(ARG_NAME);
-            paramPrice = getArguments().getString(ARG_PRICE);
-            paramDescription = getArguments().getString(ARG_DESCRIPTION);
-        }
     }
 
-    onClickListener parentActivity;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        parentActivity = (onClickListener) context;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_deal_of_day, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        dodImage = view.findViewById(R.id.deal_of_the_day_image);
-        dodCategory = view.findViewById(R.id.dod_item_catagory);
-        dodPrice = view.findViewById(R.id.dod_item_price);
-        dodName = view.findViewById(R.id.dod_item_name);
-        dodDescription = view.findViewById(R.id.dod_item_description);
+        Context context = requireContext();
 
-        dodImage.setImageResource(paramImageKey);
-        dodImage.setTag(paramImageKey);
-        dodCategory.setText(paramCategory);
-        dodPrice.setText(paramPrice);
-        dodName.setText(paramName);
-        dodDescription.setText(paramDescription);
+        MyApplication app = (MyApplication) context.getApplicationContext();
 
-        view.setOnClickListener((v) -> {
-            parentActivity.onItemClick(view);
-        });
+        ViewPager2 viewPager2Carousel = view.findViewById(R.id.carouselViewPager2);
+        CarouselViewPagerAdapter carouselViewPagerAdapter = new CarouselViewPagerAdapter(context, app.dodItems);
+
+        viewPager2Carousel.setAdapter(carouselViewPagerAdapter);
     }
 }
--- app/src/main/java/com/example/fastmart/HomeFragment.java
+++ app/src/main/java/com/example/fastmart/HomeFragment.java
@@ -1,48 +1,58 @@
 package com.example.fastmart;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
-    private String mParam1;
-    private String mParam2;
-
     public HomeFragment() {
-    }
-
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Context context = requireContext();
+        MyApplication app = (MyApplication) context.getApplicationContext();
+
+
+
+        TextView homeWelcomeTextField = view.findViewById(R.id.welcomeText);
+        String greetingMessage = "Hello! " + app.name;
+        homeWelcomeTextField.setText(greetingMessage);
+
+        getChildFragmentManager()
+                .beginTransaction()
+                .replace(R.id.dodFragment, new DealOfDayFragment())
+                .commit();
+
+        ItemsListAdapter itemsListAdapter = new ItemsListAdapter(context, app.items);
+        RecyclerView recyclerView = view.findViewById(R.id.homeItemsRecyclerView);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(context, 2));
+
+        recyclerView.setAdapter(itemsListAdapter);
+
     }
 }
