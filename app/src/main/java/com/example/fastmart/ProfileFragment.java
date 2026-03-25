b642ce24071f7c814c90ffea618042483246d2ed
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-03-25 21:44:00 +0500

    Profile Frag Implemented

--- app/src/main/java/com/example/fastmart/ProfileFragment.java
+++ app/src/main/java/com/example/fastmart/ProfileFragment.java
@@ -2,63 +2,54 @@ package com.example.fastmart;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.textfield.TextInputEditText;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextInputEditText fullNameField = view.findViewById(R.id.profile_fullName);
+        TextInputEditText emailField = view.findViewById(R.id.profile_email);
+        TextInputEditText dobField = view.findViewById(R.id.profile_dob);
+        TextInputEditText genderField = view.findViewById(R.id.profile_gender);
+        TextInputEditText phNoField = view.findViewById(R.id.profile_phNo);
+
+        MyApplication app = (MyApplication) requireContext().getApplicationContext();
+
+        fullNameField.setInputType(InputType.TYPE_NULL);
+        emailField.setInputType(InputType.TYPE_NULL);
+        dobField.setInputType(InputType.TYPE_NULL);
+        genderField.setInputType(InputType.TYPE_NULL);
+        phNoField.setInputType(InputType.TYPE_NULL);
+
+        fullNameField.setText(app.name);
+        emailField.setText(app.email);
+        dobField.setText(app.dob);
+        genderField.setText(app.gender);
+        phNoField.setText(app.phNo);
     }
 }
