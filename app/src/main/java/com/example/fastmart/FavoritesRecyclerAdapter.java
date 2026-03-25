f663be9d621db97d0a5aea1f786562b9588923b0
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-03-25 15:27:12 +0500

    Favourite Fragment added and adapter for it also addeD

--- app/src/main/java/com/example/fastmart/FavoritesRecyclerAdapter.java
+++ app/src/main/java/com/example/fastmart/FavoritesRecyclerAdapter.java
@@ -1,4 +1,33 @@
 package com.example.fastmart;
 
-public class FavoritesRecyclerAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FavoritesRecyclerAdapter extends RecyclerView.Adapter<FavoritesRecyclerAdapter.FavoritesViewHolder> {
+
+    @NonNull
+    @Override
+    public FavoritesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoritesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class FavoritesViewHolder extends RecyclerView.ViewHolder {
+
+        public FavoritesViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
